32535bdaa3f7db8c948e645ebad45a5d6b4a0348
PushkinXIII <a.v.pushkin@gmail.com>
2022-07-01 17:04:38 +0300

    Add homework_lesson_1.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-name = input("Input your name, bro:")
-print("Hello,", name)
+from decorator_timecheck import decor
+from decorator_timecheck import time_check
+
+
+@time_check
+@decor
+def printer():
+    result = sum(range(0, 100000000))
+    print(result)
+
+
+printer()
