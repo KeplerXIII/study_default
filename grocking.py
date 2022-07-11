9a40a297676b05d21fa1366776e05bada7a7e93b
PushkinXIII <a.v.pushkin@gmail.com>
2022-07-11 10:41:37 +0300

    Update grocking.py

--- grocking.py
+++ grocking.py
@@ -1,4 +1,23 @@
+# Считаем сумму списка
+
 def sum(list):
     if list == []:
         return 0
     return list[0] + sum(list[1:])
+
+# Считаем количество элементов в списке
+
+
+def count(list):
+    if list == [] :
+        return 0
+    return 1 + count(list[1:])
+
+
+# Наибольшее число в списке
+
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
