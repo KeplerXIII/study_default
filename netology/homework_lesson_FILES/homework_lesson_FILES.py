8557bba15c8c29150824f6cb42bcb2ec2a01aacb
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-16 13:03:41 +0300

    update homework_lesson_FILES

--- netology/homework_lesson_FILES/homework_lesson_FILES.py
+++ netology/homework_lesson_FILES/homework_lesson_FILES.py
@@ -38,8 +38,34 @@ def get_shop_list_by_dishes(dishes, person_count):
                     result[ingredient["ingredient_name"]]['quantity'] + \
                     ingredient['quantity'] * person_count
         else:
-            print(f"\nБлюда {dish} нет в книге рецептов.\n")
+            print(f"\nБлюда '{dish}' нет в книге рецептов.\n")
     return result
 
-
 pprint(get_shop_list_by_dishes(['Омлет', 'Глазунья', 'Торт'], 3))
+
+
+with open("1.txt", encoding='utf-8') as file_1:
+    text1_list = file_1.readlines()
+    text1_len = str(len(text1_list))
+    text1_list.insert(0, text1_len + "\n")
+    text1_list.insert(0, '1.txt\n')
+
+with open("2.txt", encoding='utf-8') as file_2:
+    text2_list = file_2.readlines()
+    text2_len = str(len(text2_list))
+    text2_list.insert(0, text2_len + "\n")
+    text2_list.insert(0, '2.txt\n')
+
+with open("3.txt", encoding='utf-8') as file_3:
+    text3_list = file_3.readlines()
+    text3_len = str(len(text3_list))
+    text3_list.insert(0, text3_len + "\n")
+    text3_list.insert(0, '3.txt\n')
+
+
+sort_list = [text1_list, text2_list, text3_list]
+sort_list.sort(key=len)
+
+with open("4.txt", "w", encoding='utf-8') as file_4:
+    file_4.write("".join(sort_list[0]) + "\n" + "".join(sort_list[1]) + "\n" + "".join(sort_list[2]))
+
