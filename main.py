e133c47d571c9b54dd0bb719016efc6097d2d167
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-16 16:25:58 +0300

    update homework_lesson_FILES

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+# with open(file1, encoding='utf-8') as file_1:
+#     text1_list = file_1.readlines()
+#     text1_len = str(len(text1_list))
+#     text1_list.insert(0, text1_len + "\n")
+#     text1_list.insert(0, file1 + '\n')
+#
+# with open(file2, encoding='utf-8') as file_2:
+#     text2_list = file_2.readlines()
+#     text2_len = str(len(text2_list))
+#     text2_list.insert(0, text2_len + "\n")
+#     text2_list.insert(0, file2 + '\n')
+#
+# with open(file3, encoding='utf-8') as file_3:
+#     text3_list = file_3.readlines()
+#     text3_len = str(len(text3_list))
+#     text3_list.insert(0, text3_len + "\n")
+#     text3_list.insert(0, file3 + '\n')
+#
+# sort_list = [text1_list, text2_list, text3_list]
+# sort_list.sort(key=len)
 
-reviewer_alex = Reviewer("Alexey", "Zazulya")
-reviewer_platon = Reviewer("Platon", "Markov")
-lector_serg = Lecture("Sergey", "Nazarov")
-lector_pavel = Lecture("Pavel", "Beliy")
-student_vasiliy = Student("Vasiliy", "Gordeev", "m")
-student_petr = Student("Petr", "Goncharov", "m")
-
-print(student_vasiliy)
-print(lector_serg)
-print(reviewer_alex)
+# with open("4.txt", "w", encoding='utf-8') as file_4:
+#     file_4.write(
+#         "".join(sort_list[0]) + "\n" + "".join(sort_list[1]) + "\n" + "".join(sort_list[2]))
--- netology/homework_lesson_FILES/homework_lesson_FILES.py
+++ netology/homework_lesson_FILES/homework_lesson_FILES.py
@@ -40,30 +40,18 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 def text_combiner(file1, file2, file3):
-    with open(file1, encoding='utf-8') as file_1:
-        text1_list = file_1.readlines()
-        text1_len = str(len(text1_list))
-        text1_list.insert(0, text1_len + "\n")
-        text1_list.insert(0, file1 + '\n')
-
-    with open(file2, encoding='utf-8') as file_2:
-        text2_list = file_2.readlines()
-        text2_len = str(len(text2_list))
-        text2_list.insert(0, text2_len + "\n")
-        text2_list.insert(0, file2 + '\n')
-
-    with open(file3, encoding='utf-8') as file_3:
-        text3_list = file_3.readlines()
-        text3_len = str(len(text3_list))
-        text3_list.insert(0, text3_len + "\n")
-        text3_list.insert(0, file3 + '\n')
-
-    sort_list = [text1_list, text2_list, text3_list]
-    sort_list.sort(key=len)
-
+    temp = [file1, file2, file3]
+    text_list =[]
+    for file in temp:
+        with open(file, encoding="utf-8") as f:
+            f_list = f.readlines()
+            f_len = str(len(f_list))
+            f_list.insert(0, f_len + "\n")
+            f_list.insert(0, file + "\n")
+            text_list.append(f_list)
+    text_list.sort(key=len)
     with open("4.txt", "w", encoding='utf-8') as file_4:
-        file_4.write(
-            "".join(sort_list[0]) + "\n" + "".join(sort_list[1]) + "\n" + "".join(sort_list[2]))
+        file_4.write("".join(text_list[0]) + "\n" + "".join(text_list[1]) + "\n" + "".join(text_list[2]))
 
 
 cook_book = cook_book_interpritator("recipes.txt")
