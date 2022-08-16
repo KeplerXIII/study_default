b9276ae8fdb5e03399e744ac930ab16f6791160c
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-16 11:06:37 +0300

    update homework_lesson_FILES

--- netology/homework_lesson_FILES/homework_lesson_FILES.py
+++ netology/homework_lesson_FILES/homework_lesson_FILES.py
@@ -1,24 +1,28 @@
 from pprint import pprint
 
-def cook_book():
-    with open("recipes.txt", encoding="utf-8") as file:
-        cook_book = {}
+
+def cook_book_interpritator(recipes_file):
+    with open(recipes_file, encoding="utf-8") as file:
+
+        book = {}
 
         for line in file:
             dish = line.strip()
             quantity = file.readline().strip()
             recipe = []
 
-            for line in range(int(quantity)):
+            for ingredients in range(int(quantity)):
                 text = file.readline().split(" | ")
-                recipe.append({'ingredient_name': text[0], 'quantity': int(text[1]),
-                               'measure': text[2].strip()})
-            cook_book[dish] = recipe
+                recipe.append(
+                    {'ingredient_name': text[0], 'quantity': int(text[1]),
+                     'measure': text[2].strip()})
+            book[dish] = recipe
             file.readline()
-    return cook_book
+    return book
 
 
-cook_book = cook_book()
+cook_book = cook_book_interpritator("recipes.txt")
+
 
 def get_shop_list_by_dishes(dishes, person_count):
     result = {}
@@ -26,13 +30,16 @@ def get_shop_list_by_dishes(dishes, person_count):
         if dish in cook_book:
             for ingredient in cook_book[dish]:
                 if ingredient["ingredient_name"] not in result:
-                    result[ingredient["ingredient_name"]] = {'measure': ingredient["measure"], 'quantity': ingredient['quantity'] * person_count}
+                    result[ingredient["ingredient_name"]] = {
+                        'measure': ingredient["measure"],
+                        'quantity': ingredient['quantity'] * person_count}
                 else:
-                    result[ingredient["ingredient_name"]]['quantity'] = result[ingredient["ingredient_name"]]['quantity'] + ingredient['quantity'] * person_count
+                    result[ingredient["ingredient_name"]]['quantity'] = \
+                    result[ingredient["ingredient_name"]]['quantity'] + \
+                    ingredient['quantity'] * person_count
         else:
-            print(f"Блюда {dish} нет в книге рецептов.")
+            print(f"\nБлюда {dish} нет в книге рецептов.\n")
     return result
 
 
 pprint(get_shop_list_by_dishes(['Омлет', 'Глазунья', 'Торт'], 3))
-
