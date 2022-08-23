459aca8657c16ae7745598e08b0675cd0752b8fa
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-23 10:09:24 +0300

    update homework_lesson_Request

--- netology/homework_lesson_requests/overflow.py
+++ netology/homework_lesson_requests/overflow.py
@@ -7,7 +7,12 @@ class Overflow:
     def two_days_question(self):
         url = "https://api.stackexchange.com/2.3/questions"
         date = str(round(time.time()) - 172800)
-        params = {"fromdate": date, "order": "desc", "sort": "creation", "tagged": "Python", "site": "stackoverflow", "pagesize": "100"}
+        params = {"fromdate": date,
+                  "order": "desc",
+                  "sort": "creation",
+                  "tagged": "Python",
+                  "site": "stackoverflow",
+                  "pagesize": "100"}
         response = requests.get(url, params=params)
         response = response.json()
         i = 0
