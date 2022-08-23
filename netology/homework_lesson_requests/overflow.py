fc2e0dc8071c68d5502e46b8e1c01c63ea3d6112
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-23 11:06:52 +0300

    update homework_lesson_Request

--- netology/homework_lesson_requests/overflow.py
+++ netology/homework_lesson_requests/overflow.py
@@ -4,19 +4,28 @@ from datetime import datetime
 
 class Overflow:
 
-    def two_days_question(self):
+    def two_days_question(self, page=1, i=0):
         url = "https://api.stackexchange.com/2.3/questions"
         date = str(round(time.time()) - 172800)
+        self.page = page
+        self.i = i
         params = {"fromdate": date,
                   "order": "desc",
                   "sort": "creation",
                   "tagged": "Python",
                   "site": "stackoverflow",
-                  "pagesize": "100"}
+                  "pagesize": "100",
+                  "page": page}
+
         response = requests.get(url, params=params)
         response = response.json()
-        i = 0
+        has_more = response['has_more']
+        page += 1
         for item in response["items"]:
             i += 1
-            print(datetime.fromtimestamp(item["creation_date"]).strftime("%A, %B %d, %Y %I:%M:%S"), item["title"])
-        print(f'Количество строк {i}')
+            print(datetime.fromtimestamp(item["creation_date"]).strftime("%A, %B %d, %Y %I:%M:%S"),
+                  item["title"])
+        if has_more:
+            self.two_days_question(page + 1, i)
+        else:
+            print(f'Количество вопросов за 2 дня - {i}')
